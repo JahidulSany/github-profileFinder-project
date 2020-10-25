63c12a196be068d20256674c8804de28577ad7f5
Jahidul Islam Sany <jahidulsanypro@gmail.com>
2020-10-26 00:55:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+/* Happy coding 🥰🤠🥳🥳💯💯
+fetch(`https://api.github.com/users/andrew`)
+    .then(res => res.json())
+    .then(profile => console.log(profile));
+*/
+const CLIENT_ID = 'Iv1.120feceb0a63a902';
+const CLIENT_SECRET = 'f2b1a0789aa3baf2fa60e70d90424f5219c1cbf3';
+
+async function getUser(name){
+   const res = await fetch(`https://api.github.com/users/${name}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`);
+
+   const profile = await res.json();
+
+   return profile;
+}
+
+async function getRepos(profile){
+    const res = await fetch(`${profile.repos_url}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&per_page=10`);
+
+    const repo = await res.json();
+
+    return repo;
+}
+
+document.querySelector('#search').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.querySelector('#findByUsername').value;
+
+    if(username.length > 0){
+
+        document.querySelector('.loader').style.display = 'block';
+        document.querySelector('.user-details').style.display = 'none';
+
+        const profile = await getUser(username);
+
+        document.querySelector('.loader').style.display = 'none';
+        
+        if(profile.message === 'Not Found') {
+            document.querySelector('.notFound').style.display = 'block';
+        }else{
+            const repos = await getRepos(profile);
+            document.querySelector('.user-details').style.display = 'flex';
+            showProfile(profile);
+            showRepos(repos);
+            document.querySelector('#findByUsername').value = '';
+        }
+    }
+});
+
+function showProfile(profile){
+    document.querySelector('.profile').innerHTML = 
+    `   <img 
+        src=${profile.avatar_url}
+        alt=${profile.name}/>
+        <p class="name">${profile.name}</p>
+        <p class="username login">${profile.login}</p>
+        <p class="bio">
+        ${profile.bio}
+        </p>
+
+        <div class="followers-stars">
+            <p>
+                <ion-icon name="people-outline"></ion-icon>
+                <span class="followers"> ${profile.followers} </span> followers
+            </p>
+            <span class="dot">·</span>
+            <p><span class="following"> ${profile.following} </span> following</p>
+        </div>
+
+        <p class="company">
+            <ion-icon name="business-outline"></ion-icon>
+            ${profile.company}
+        </p>
+        <p class="location">
+            <ion-icon name="location-outline"></ion-icon>${profile.location}
+        </p>
+    `;
+}
+
+
+function showRepos(repos){
+    let newHTML = '';
+    for(let repo of repos){
+        newHTML += `
+            <div class="repo">
+                <div class="repo_name">
+                    <a href="${repo.html_url}">${repo.name}</a>
+                </div>
+                <p>
+                    <span class="circle"></span> ${repo.language}
+                    <ion-icon name="star-outline"></ion-icon> ${repo.watchers}
+                    <ion-icon name="git-branch-outline"></ion-icon> ${repo.forks_count}
+                </p>
+            </div>
+        `;
+    }
+    document.querySelector('.repos').innerHTML = newHTML;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
